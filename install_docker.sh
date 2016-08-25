26a558c0490e8256c6a4d25c9cc4d488e4e46965
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-08-25 11:33:14 -0400

    Change echo at end of script to prompt restart

--- install_docker.sh
+++ install_docker.sh
@@ -3,4 +3,4 @@ curl -s https://packagecloud.io/install/repositories/Hypriot/Schatzkiste/script.
 sudo apt-get install -y docker-hypriot docker-compose
 sudo usermod -aG docker pi
 sudo systemctl enable docker.service
-echo Finished installing docker. You may have to start a new terminal session for all changes to take effect
+echo Finished installing docker. You may have to restart for all changes to take effect
